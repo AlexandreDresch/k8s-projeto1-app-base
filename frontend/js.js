c7cc6d4410617d422e45ae88dbd40cc2a62f3753
AlexandreDresch <connectWithAlexandre@proton.me>
2025-07-18 21:57:30 -0300

    fix: Update AJAX request URL in js.js for proper data submission

--- frontend/js.js
+++ frontend/js.js
@@ -1,21 +1,27 @@
-$("#button-blue").on("click", function() {
-    
-    var txt_nome = $("#name").val();
-    var txt_email = $("#email").val();
-    var txt_comentario = $("#comment").val();
+$("#button-blue").on("click", function () {
+  var txt_nome = $("#name").val();
+  var txt_email = $("#email").val();
+  var txt_comentario = $("#comment").val();
 
-    $.ajax({
-        url: "",
-        
-        type: "post",
-        data: {nome: txt_nome, comentario: txt_comentario, email: txt_email},
-        beforeSend: function() {
-        
-            console.log("Tentando enviar os dados....");
+  $.ajax({
+    url: "http://php-backend-service/index.php",
 
-        }
-    }).done(function(e) {
-        alert("Dados Salvos");
+    type: "post",
+    data: { nome: txt_nome, comentario: txt_comentario, email: txt_email },
+    beforeSend: function () {
+      console.log("Tentando enviar os dados....");
+    },
+  })
+    .done(function (e) {
+      alert("Dados Salvos");
     })
-
+    .fail(function (jqXHR, textStatus, errorThrown) {
+      console.error(
+        "Erro ao enviar dados:",
+        textStatus,
+        errorThrown,
+        jqXHR.responseText
+      );
+      alert("Erro ao salvar dados. Verifique o console.");
+    });
 });
